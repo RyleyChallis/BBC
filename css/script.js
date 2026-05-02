9b308e1a71db57e74062a64f547a2eb1104169d6
Ryley C <ryleychallis15@gmail.com>
2026-05-02 18:24:07 +0100

    Fresh start from root

---
+++ css/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById('sidebar').classList.add("active");
+    document.getElementById('sidebar').style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById('sidebar').classList.remove("active");
+    document.getElementById('sidebar').style.display = "none";
+}
